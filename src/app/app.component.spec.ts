c4191e49251de462f232b57bb0b689f0405951f8
Emily Pancake <emily.pancake@pinnacol.com>
2018-07-02 16:43:02 -0600

    added more tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -41,4 +41,24 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     expect(component.title).toEqual('Picture Book');
   }));
+  it('should have a list of 10 photos', async(() => {
+    fixture.detectChanges();
+    component.performSearch('banana');
+    // tslint:disable-next-line:max-line-length
+    backend.expectOne(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=236ee9bd3afee7904f32fae37ab4ea2f&format=json&nojsoncallback=1&tags=banana`)
+           .flush(FLICKR_REPONSE);
+    expect(component.tenPhotos.length).toEqual(10);
+    expect(component.tenPhotosTitles.length).toEqual(10);
+    backend.verify();
+  }));
+  it('should have a list of 10 photos that does not match the order of the 10 titles', async(() => {
+    fixture.detectChanges();
+    component.performSearch('banana');
+    // tslint:disable-next-line:max-line-length
+    backend.expectOne(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=236ee9bd3afee7904f32fae37ab4ea2f&format=json&nojsoncallback=1&tags=banana`)
+           .flush(FLICKR_REPONSE);
+    expect(component.tenPhotos[0].title === component.tenPhotosTitles[0].title
+      && component.tenPhotos[1].title === component.tenPhotosTitles[1].title).toBeFalsy();
+    backend.verify();
+  }));
 });
